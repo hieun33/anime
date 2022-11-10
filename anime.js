13f5d473e1473109f370c9daeadff54ae59c9507
hieun33 <grace3029@gmail.com>
2022-11-10 11:52:16 +0900

    플러스마이너스함수 run수로 통합

--- anime.js
+++ anime.js
@@ -1,51 +1,31 @@
 const btn = document.querySelector('button');
 const box = document.querySelector('#box');
-
 btn.addEventListener('click', () => {
   anime(box, {
     prop: 'margin-left',
-    value: 100,
+    value: 500,
     duration: 500
-   
   })
 });
 
 function anime(selector, option) {
   const startTime = performance.now();
   const currentValue = parseInt(getComputedStyle(selector)[option.prop]);
-  if (option.value === currentValue) return;
-  if (option.value > currentValue) requestAnimationFrame(plus);
-  if (option.value < currentValue) requestAnimationFrame(minus);
+  (option.value !== currentValue) && requestAnimationFrame(run);
 
-  
-
-  function plus(time) {
+  function run(time) {
     let timelast = time - startTime;
     let progress = timelast / option.duration;
 
     (progress < 0) && (progress = 0);
     (progress > 1) && (progress = 1);
     if (progress < 1) {
-      requestAnimationFrame(plus);
+      requestAnimationFrame(run);
     } else {
       option.callback && option.callback();
     }
-    let result = currentValue + ((option.value - currentValue) * progress)
+    let result = currentValue + ((option.value - currentValue) * progress);
     selector.style[option.prop] = `${result}px`;
   }
 
-    function minus(time) {
-        let timelast = time - startTime;
-        let progress = timelast / option.duration;
-    
-        (progress < 0) && (progress = 0);
-        (progress > 1) && (progress = 1);
-        if (progress < 1) {
-          requestAnimationFrame(plus);
-        } else {
-          option.callback && option.callback();
-        }
-        let result = currentValue - ((currentValue - option.value) * progress)
-        selector.style[option.prop] = `${result}px`;
-      }
-    }
+}
